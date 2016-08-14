f1d6d7cba2ce23eef6c81edbf3ce8ab35151a9c8
pooh <pythongirl@users.noreply.github.com>
2016-08-14 03:00:03 -0600

    connects to rtr1 and 2 to get SNMP MIB for name and descr

--- c2e4c.py
+++ c2e4c.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 '''
 Create a script that connects to both routers (pynet-rtr1 and pynet-rtr2) and prints out both the MIB2 sysName and sysDescr.
 '''
@@ -24,3 +26,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
