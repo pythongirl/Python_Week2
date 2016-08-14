2f14fa3f259997514b0a107fc7b3e451cd751036
pythongirl <vinita.shah@colorado.edu>
2016-08-14 00:43:03 -0700

    module containing a hello func

---
+++ my_func.py
@@ -0,0 +1,10 @@
+'''
+Write a simple Python module that contains one function that prints 'hello' (module name = my_func.py). Do a test where you impor my_func into a new Python script. Test this using the following contexts:
+        * my_func.py is located in the same directory as your script
+        * my_func.py is located in some random subdirectory (not the same directory as your script)
+        * my_func.py is located in ~/applied_python/lib/python2.7/site-packages/
+'''
+
+def print_hello():
+    print "Hello!!"
+
